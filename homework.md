2fd9bff9e4a89ffce79d5dedbad42e70d8308f19
KhadangRohit <rohitkhadang838@gmail.com>
2026-01-02 21:38:16 +0530

    Explore get ,post , delete

--- homework.md
+++ homework.md
@@ -6,3 +6,16 @@
 - Listen to port 3000
 - Write request handlers for /test, /hello
 - Install nodemon and update scripts inside package.json
+
+- Initialize git
+- gitignore
+-create a remote repo on github
+- push all code to remote origin
+
+- You have to play with route extension
+- ex /hello, /hello/2,/xyz
+- Order of the routes is matter
+
+-Install Postman App and make a workspace/collection > test API call
+
+-write logic to handle get,post,patch,delete API calls  and test them on Postman
--- src/app.js
+++ src/app.js
@@ -1,14 +1,24 @@
  const express = require('express');
 
  const app = express();
+
+ app.get("/user", (req,res) =>{
+   res.send({fistName:"Rohit", lastName:"Khadang"});
+ });
  
- app.use("/hello",(req,res) =>{
-    res.send("Hello,Hello from the server 3000");
+ app.post("/user", (req,res) => {
+   res.send("Data successfully saved to the database");
  });
 
+ app.delete("/user", (req,res) => {
+   res.send("Deleted successfully");
+ });
+ 
  app.use("/test",(req,res) =>{
     res.send("Hello, from  the test server 3000");
  });
+
+  
 
  app.listen(3000, () =>{
     console.log("Server is successfully on port 3000 ...");
